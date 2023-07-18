20a85f5a897440778d47086899a118c5a6f451e2
Steven Shelton <steven.d.shelton@gmail.com>
2023-07-18 10:16:28 -0400

    updates LGAtoZExport to includes subject lists

--- LGAtoZExport.sql
+++ LGAtoZExport.sql
@@ -1,35 +1,40 @@
 SELECT
 Vendor.VendorName AS vendor,
-Dbases.Title AS name,
-Dbases.URL AS url,
-(CASE Dbases.NotProxy  WHEN 1 THEN 0 ELSE 1 END) AS enable_proxy,
-Dbases.ShortDescription AS description,
-Dbases.HighlightedInfo AS more_info,
-Dbases.`New` AS enable_new,
+db.Title AS name,
+db.URL AS url,
+(CASE db.NotProxy  WHEN 1 THEN 0 ELSE 1 END) AS enable_proxy,
+db.ShortDescription AS description,
+db.HighlightedInfo AS more_info,
+db.`New` AS enable_new,
 0 AS enable_trial,
-Dbases.ContentType AS types,
+db.ContentType AS types,
 '' AS keywords,
 0 AS target,
 CASE
-  WHEN Dbases.ShortURL IS NULL THEN
-  LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(Dbases.Title, ' ', '-'), '(', ''),')',''),'.',''),':','_'),',','_'))
+  WHEN db.ShortURL IS NULL THEN
+  LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(db.Title, ' ', '-'), '(', ''),')',''),'.',''),':','_'),',','_'))
   ELSE
-  Dbases.ShortURL
+  db.ShortURL
 END
 AS slug,
 '' AS best_bets,
-'' AS subjects,
+GROUP_CONCAT(SubjectList.Subject separator ';') AS subjects,
 1 AS desc_pos,
-Dbases.PrivateNotes AS lib_note,
+'' AS lib_note,
 0 AS enable_popular,
-Dbases.MASKED AS enable_hidden,
-'' AS internal_note,
+db.MASKED AS enable_hidden,
+db.PrivateNotes AS internal_note,
 '' AS owner, 
 '' AS resource_icons,
 '' AS thumbnail
-FROM LuptonDB.Dbases
+FROM LuptonDB.Dbases AS db
 JOIN LuptonDB.Vendor
-on Dbases.Vendor_ID = Vendor.Vendor_ID
-WHERE CANCELLED = 0 AND MASKED = 0
-ORDER BY Dbases.Key_ID 
+ON db.Vendor_ID = Vendor.Vendor_ID
+JOIN LuptonDB.DBRanking
+ON db.Key_ID  = DBRanking.Key_ID
+JOIN LuptonDB.SubjectList
+ON DBRanking.Subject_ID = SubjectList.Subject_ID
+WHERE CANCELLED = 0 AND MASKED = 0 AND db.Key_ID > 200
+GROUP BY db.Title
+ORDER BY db.Key_ID 
 ;
